6ff3e652dc0ccc23c71b1340dc004b673080fba1
sarel.saban <sarel.saban@gmail.com>
2014-05-17 17:07:33 -0700

    add nbproject and static dir

--- dj_TeamWork_project/dj_TeamWork_project/settings.py
+++ dj_TeamWork_project/dj_TeamWork_project/settings.py
@@ -1,5 +1,13 @@
 # Django settings for dj_TeamWork_project project.
 
+import os
+SETTINGS_DIR = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+STATIC_PATH = os.path.join(PROJECT_PATH,'static')
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -72,6 +80,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +120,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+    
 )
 
 INSTALLED_APPS = (
